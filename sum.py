761b3b6bc12b1f316266eb253ed43802f5123cc3
Almas Temirbekov <almas_tem@mail.ru>
2024-02-15 18:45:39 +0600

    3

--- sum.py
+++ sum.py
@@ -5,6 +5,6 @@ def product(a, b):
         return (a + product(a, b-1))
     else:
         return 0
-a = int(input("Введите первое число: "))
-b = int(input("Введите второе число: "))
+a = int(input("Diведите первое число: "))
+b = int(input("Diведите второе число: "))
 print("Произведением двух чисел будет: ", product(a, b))
